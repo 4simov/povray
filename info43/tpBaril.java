41b72095db67ebde48788a2508e38bc87cc4b27a
4simov <cc00@laposte.net>
2018-04-04 08:06:42 +0200

    Add files via upload

--- info43/tpBaril.java
+++ info43/tpBaril.java
@@ -2,8 +2,9 @@ public class tpBaril{
 	
 	static String[] tab;
 	static String a,b;//pour la division
-	final static int n=5;
+	final static int n=4;
 	static int longueur;	//taille tableau
+	static int compteur;
 	static int i;
 	
 	public tpBaril(){
@@ -12,6 +13,7 @@ public class tpBaril{
 			a="";
 			b="";
 			i=0;
+			compteur=1;
 	}
 	
 	//sqce binaire
@@ -32,35 +34,72 @@ public class tpBaril{
 			a = s.substring(0, indice+1);
 			b = s.substring(indice+1);
 	}
+	
+
+	
 	public static void Tri() {
 			int i,j;
 			for(j=1; j<longueur; j++){
-			for( i=0; i<(n-1); i++){
 				if(tab[j] !="2") {
+					
+				for( i=0; i<(n-1); i++){
 					decoupe( tab[j], i);
 					System.out.println(""+a+" "+b);
-					if( tab[i].equals( b+a)){
-						tab[i] = "2";				
+					for (int k=j+1;k<tab.length;k++){
+						if( tab[k].equals( b+a)){
+							tab[k] = "2";				
+						}
+						
+											
+						
 					}
 				}
+				compteur++;
 			}
 		}
 	}
 	
+public static void periodique(){
+
+			
+		if(n%2==1){	//si i n'est pas un diviseur de n		
+			Tri();
+		}
+		else{
+		
+			for(int i=1;i<=n;i++){
+				if(n%i==0){	//si i est un diviseur de n		
+					if(multidecoupe(tab[i], i) ){
+						tab[i] = "2";					
+					}
+				}
+			}
+			
+		}
+	
+}
+	
 	public static void affiche(){
 		for(int i=0;i<longueur;i++){
-			
-			System.out.print(" "+tab[i]);		
+			if(tab[i].equals("2")){
+				System.out.print("");		
+			}
+			else {
+				System.out.print(" "+tab[i]);		
+			}
 		}	
-		System.out.println("");		
+		System.out.println("\n compteur : "+compteur);		
 	}
+	
+	
 	
 	public static void main(String arg[]){
 	//	System.exit(0);
 
 		tpBaril t=new tpBaril();
 		GenBean("",n);
-		Tri();
+		//Tri();
+		periodique();
 		affiche();
 			
 	}
